d1e4b959866e14d0ae19e8a0d1de74e56254fa8b
Даниил <daniildaniil2107@gmail.com>
2025-09-14 11:10:46 +0300

    modified:   z1.py 	new file:   "\320\221\320\265\320\267 \320\270\320\274\320\265\320\275\320\270 1.ods"

--- z1.py
+++ z1.py
@@ -1,6 +1,39 @@
 import time
 import random
 
+import matplotlib as plt
+
+def shell(nums):
+    inc = len(nums) // 2
+    while inc:
+        for i, el in enumerate(nums):
+            while i >= inc and nums[i - inc] > el:
+                nums[i] = nums[i - inc]
+                i -= inc
+            nums[i] = el
+        inc = 1 if inc == 2 else int(inc * 5.0 / 11)
+    return nums
+
+def built_in_sort(nums):
+    return sorted(nums)
+
+def quicksort(nums):
+   if len(nums) <= 1:
+       return nums
+   else:
+       q = random.choice(nums)
+       s_nums = []
+       m_nums = []
+       e_nums = []
+       for n in nums:
+           if n < q:
+               s_nums.append(n)
+           elif n > q:
+               m_nums.append(n)
+           else:
+               e_nums.append(n)
+       return quicksort(s_nums) + e_nums + quicksort(m_nums)
+
 def fill_matr(matr):
     for i in matr:
         for j in range(len(i)):
@@ -43,24 +76,30 @@ def test(razm):
     print(f"время премножения матриц {razm}X{razm} составило {time_of_work} секунд")
     return time_of_work
     
+def draw(X, Y):
+    plt.plot(X, Y)
+    plt.xlabel("Размерности матриц")
+    plt.ylabel("Время работы")
+    plt.title("Перемножение матриц") 
+    plt.show()
 
 
-rows_cols = [100]
-while len(rows_cols)!=19:
+if __name__=="__main__":
+    rows_cols = [100]
+    while len(rows_cols)!=19:
 
-    if rows_cols[len(rows_cols)-1]<1000:
-        rows_cols.append(rows_cols[len(rows_cols)-1]+100)
+        if rows_cols[len(rows_cols)-1]<1000:
+            rows_cols.append(rows_cols[len(rows_cols)-1]+100)
 
-    else: rows_cols.append(rows_cols[len(rows_cols)-1]+1000)
+        else: rows_cols.append(rows_cols[len(rows_cols)-1]+1000)
 
 # print(rows_cols)
 
 
-itogo = []
+    itogo = []
 
-for i in rows_cols:
-    if i==1000:break
-    else:itogo.append(round(test(i), 5))
+    for i in rows_cols:
+        if i==5000:break
+        else:itogo.append(round(test(i), 5))
 
-    
-print(itogo)
+    print(itogo, sum(itogo)/3600)
